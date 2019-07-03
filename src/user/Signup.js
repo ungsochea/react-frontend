5d795062ea18a2f5681427206a5c932e43bb5029
ungsochea <sochea.ung@gmail.com>
2019-07-03 21:15:50 +0700

    clean code form signup

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -57,13 +57,7 @@ class Signup extends Component {
         .catch(err=>console.log(err))
     }
 
-    render(){
-        const {name,email,password,error,open} = this.state;
-        return (
-            <div className="container">
-                <h2 className="mt-5 mb-5">Signup</h2>
-                <div className="alert alert-primary" style={{ display: error ? " ":"none" }}>{ error }</div>
-                <div className="alert alert-info" style={{ display: open ? " ":"none" }}>New account is successfully. Please go to signin</div>
+    signupForm = (name,email,password) => (
                 <form>
                     <div className="form-group">
                         <label className="text-muted">Name</label>
@@ -79,6 +73,16 @@ class Signup extends Component {
                     </div>
                     <button onClick={this.clickSubmit} className="btn btn-raised btn-primary"> Submit</button>
                 </form>
+    )
+
+    render(){
+        const {name,email,password,error,open} = this.state;
+        return (
+            <div className="container">
+                <h2 className="mt-5 mb-5">Signup</h2>
+                <div className="alert alert-danger" style={{ display: error ? " ":"none" }}>{ error }</div>
+                <div className="alert alert-info" style={{ display: open ? " ":"none" }}>New account is successfully. Please go to signin</div>
+                {this.signupForm(name,email,password)}
             </div>
         )
     }

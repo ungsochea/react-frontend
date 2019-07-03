ded3c117bbc3036244eccae16d5e45234ed21bb4
ungsochea <sochea.ung@gmail.com>
2019-07-03 21:11:06 +0700

    form signup , signup with api, message error and success

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -8,21 +8,62 @@ class Signup extends Component {
             name:"",
             email:"",
             password:"",
-            error:""
+            error:"",
+            open:false
         }
     }
 
     handleChange = name => (event) => {
+        this.setState({error:""})
         this.setState({
             [name]:event.target.value
         })
     }
 
-    render(){
+    clickSubmit = event => {
+        event.preventDefault();
         const {name,email,password} = this.state;
+        const user = {
+            name,
+            email,
+            password,
+        };
+        // console.log(user)
+        this.signup(user)
+        .then(data => {
+            if(data.error) this.setState({error:data.error})
+                else this.setState({
+                    name:"",
+                    email:"",
+                    password:"",
+                    error:"",
+                    open:true
+                })
+        })
+    };
+
+    signup = (user) => {
+        return fetch("http://localhost:8080/signup",{
+            method:"POST",
+            headers:{
+                Accept:"application/json",
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify(user)
+        })
+        .then(response =>{
+            return response.json();
+        })
+        .catch(err=>console.log(err))
+    }
+
+    render(){
+        const {name,email,password,error,open} = this.state;
         return (
             <div className="container">
                 <h2 className="mt-5 mb-5">Signup</h2>
+                <div className="alert alert-primary" style={{ display: error ? " ":"none" }}>{ error }</div>
+                <div className="alert alert-info" style={{ display: open ? " ":"none" }}>New account is successfully. Please go to signin</div>
                 <form>
                     <div className="form-group">
                         <label className="text-muted">Name</label>
@@ -36,7 +77,7 @@ class Signup extends Component {
                         <label className="text-muted">Password</label>
                         <input onChange={this.handleChange("password")}  type="password" className="form-control" value={password}/>
                     </div>
-                    <button className="btn btn-raised btn-primary"> Submit</button>
+                    <button onClick={this.clickSubmit} className="btn btn-raised btn-primary"> Submit</button>
                 </form>
             </div>
         )

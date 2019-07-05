406e0b6d295d95c3163665e21ca6730c27cb1045
ungsochea <sochea.ung@gmail.com>
2019-07-05 11:34:59 +0700

    list all users

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -15,6 +15,9 @@ const Menu = ({ history }) => (
             <li className="nav-item">
                 <Link className="nav-link" style={isActive(history,"/")} to="/">Home</Link>
             </li>
+            <li className="nav-item">
+                <Link className="nav-link" style={isActive(history,"/users")} to="/users">Users</Link>
+            </li>
             {!isAuthenticated() && (
                 <>
                     <li className="nav-item">

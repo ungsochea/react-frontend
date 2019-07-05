68f01b06b287cb034b433f0cadf4258d7e53bc2e
ungsochea <sochea.ung@gmail.com>
2019-07-05 11:54:35 +0700

    Style card user and default profile

--- src/user/Users.js
+++ src/user/Users.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { list } from './apiUser'
+import DefaultProfile from '../images/user-avatar.jpg'
 
 class Users extends Component {
     constructor(){
@@ -19,23 +20,37 @@ class Users extends Component {
         })
     }
 
+    renderUser = users =>(
+        <div className="row">
+                {
+                    users.map((user,i)=>(
+                        <div className="card col-md-4" key={i}>
+                            <img className="card-img-top" src={ DefaultProfile } alt={ user.name } style={{ width:'100%',height:'15vw',objectFit:'cover' }} />
+                            <div className="card-body">
+                                <h5 className="card-title">{ user.name }</h5>
+                                <p className="card-text">
+                                    { user.email }
+                                </p>
+                                <a href="/" className="btn btn-raised btn-sm btn-primary">
+                                    View Profile
+                                </a>
+                            </div>
+                        </div>
+                    ))
+                }
+        </div>
+    )
+
     render() {
         const { users } = this.state;
         return (
             <div className="container">
                 <h2 className="mt-5 mb-5">Users</h2>
-                <div className="card">
-                     {
-                         users.map((user,i)=>{
-                            return <div key={i}>
-                                <p>{ user.name }</p>
-                             </div>
-                         })
-                     }
-                </div>
+                    {this.renderUser(users)}
             </div>
         )
     }
 }
 
 export default Users;
+
